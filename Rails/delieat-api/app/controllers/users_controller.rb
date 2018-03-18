41d394a534960635b55972406473cbd74e06b18b
Yuhao Wu <yhao.wu@gmail.com>
2018-03-17 23:07:36 -0700

    Add User model and simple login function in Rails

---
+++ Rails/delieat-api/app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+    # disable CSRF until a token authentication is implemented
+    skip_before_action :verify_authenticity_token
+
+    def index
+        @users = User.all
+
+        render :json => @users
+    end
+
+    def register
+        @user = User.new(user_params)
+
+        @user.save
+    end
+
+    def login
+        @user = User.find_by(username: params[:username], password: params[:password])
+
+        if @user
+            render plain: @user[:user_type]
+        else
+            render plain: 'false'
+        end
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:username, :password, :user_type)
+        end
+end
--- Rails/delieat-api/config/routes.rb
+++ Rails/delieat-api/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'register', to: 'users#register'
+  post 'login', to: 'users#login'
+
 end
