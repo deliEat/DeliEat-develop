279a2cf50eb8a584d6bd51f53cb60850ced33344
Kayci Wang <425021899@qq.com>
2018-03-21 23:34:51 -0700

    Added some controller methods

--- Rails/delieat-api/app/controllers/menu_item_controller.rb
+++ Rails/delieat-api/app/controllers/menu_item_controller.rb
@@ -1,2 +1,19 @@
 class MenuItemController < ApplicationController
+
+  def create
+    @menu_item = MenuItem.new(create_params)
+
+    @menu_item.save
+  end
+
+  def show
+    @menu_item = MenuItem.find(params[:id])
+
+    render :json => @menu_item
+  end
+
+  private
+  def create_params
+    params.require(:menu_item).permit(:menu_id, :name, :price, :description)
+  end
 end
