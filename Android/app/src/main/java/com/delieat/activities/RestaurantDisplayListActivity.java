7c5f1d8a2e26049aafe9c5d187042c8a1d26281b
Kayci Wang <425021899@qq.com>
2018-04-05 22:57:08 -0700

    Added a listView for viewing restaurants

--- Android/app/src/main/java/com/delieat/activities/RestaurantDisplayListActivity.java
+++ Android/app/src/main/java/com/delieat/activities/RestaurantDisplayListActivity.java
@@ -1,38 +1,45 @@
 package com.delieat.activities;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.delieat.adapters.RestaurantsAdapter;
 import com.delieat.models.Restaurant;
 
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RestaurantDisplayListActivity extends AppCompatActivity {
-    private List<Restaurant> restaurants = new ArrayList<>();
+    private ArrayList<Restaurant> restaurants;
+    private RestaurantsAdapter rAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_display_list);
-        Restaurant res = new Restaurant();
-        res.setName("blabla");
-        restaurants.add(res);
-        displayRestaurant();
+        restaurants = new ArrayList<Restaurant>();
+        rAdapter = new RestaurantsAdapter(this, restaurants);
+        ListView listView = (ListView) findViewById(R.id.restaurants);
+        listView.setAdapter(rAdapter);
+        addRestaurants();
     }
 
-    private void displayRestaurant() {
-        ScrollView scrollView = findViewById(R.id.restaurants);
-        for (Restaurant r: restaurants) {
-            TextView text = new TextView(this);
-            text.setText(r.getName());
-            scrollView.addView(text);
+    private void addRestaurants() {
+        for (Restaurant r : restaurants) {
+            rAdapter.add(r);
         }
     }
-
 
 }
