d8a9467e26984252e8c665ac915db9acf8788a5c
Kayci Wang <425021899@qq.com>
2018-06-20 23:43:57 -0700

    Updated restaurant detail page with item and can dynamically count item add

--- FlutterApp/lib/screens/restaurantDetail.dart
+++ FlutterApp/lib/screens/restaurantDetail.dart
@@ -67,6 +67,7 @@ class _RestaurantBanner extends StatelessWidget {
         ),
       ),
       decoration: new BoxDecoration(
+        color: deliEatBackgroundWhite,
         image: new DecorationImage(
           image: new AssetImage('assets/food.jpg'),
           fit: BoxFit.fill,
@@ -74,6 +75,12 @@ class _RestaurantBanner extends StatelessWidget {
             Colors.black.withOpacity(0.2), BlendMode.dstATop),
         ),
         border: new Border(bottom: new BorderSide(color: Theme.of(context).dividerColor)),
+        boxShadow: [
+          new BoxShadow(
+            color: deliEatGrey,
+            blurRadius: 5.0,
+          ),
+        ],
       ),
     );
   }
@@ -117,6 +124,7 @@ class _BottomBarState extends State<_BottomBar> {
         ),
       decoration: new BoxDecoration(
         border: new Border(top: new BorderSide(color: Theme.of(context).dividerColor)),
+        color: deliEatBackgroundWhite,
       ),
     );
   }
@@ -146,8 +154,14 @@ class _MenuSectionState extends State<_MenuSection> {
         children: <Widget>[
           new Text(
             "Menu:",
+            style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+            ),
           ),
           new Divider(),
+          new MenuCategoryView(),
+          new MenuCategoryView(),
           new MenuCategoryView(),
         ],
       ),
